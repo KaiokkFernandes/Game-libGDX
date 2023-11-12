1632c1c0b2bb1c022e7bf158acdc5e0b7208d6bf
Kaio vittor <kaiovittorg@gmail.com>
2023-11-12 03:14:48 -0300

    Adicionando fundo e funcionalidades da nave

--- core/src/com/test/game/MyGdxGame.java
+++ core/src/com/test/game/MyGdxGame.java
@@ -4,22 +4,38 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture img, tNave;
+	private Sprite nave;
+	private  float posX, posY, velocity;
+
+
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("bg.png");
+		tNave = new Texture("Spaceship.png");
+		nave = new Sprite(tNave);
+		posX = 0;
+		posY = 0;
+		velocity = 100;
 	}
 
 	@Override
 	public void render () {
+		this.moveNave();
+
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(nave,  posX, posY);
 		batch.end();
 	}
 	
@@ -27,5 +43,32 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		tNave.dispose();
+	}
+
+	private void moveNave(){
+      if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)){
+		  if( posX < Gdx.graphics.getWidth() - nave.getWidth()){
+			  posX += velocity;
+		  }
+
+	  }
+	  if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)){
+          if(  posX > 0){
+			  posX -= velocity;
+		  };
+	  }
+	  if(Gdx.input.isKeyJustPressed(Input.Keys.UP)){
+		  if(posY < Gdx.graphics.getHeight() - nave.getHeight()){
+			  posY += velocity;
+		  }
+
+		}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)){
+			if(posY > 0){
+				posY -= velocity;
+			}
+		}
+
 	}
 }
